4b25da7f69627697e25da35cf8d8061fa523d3e8
se0jin <tina030917@gmail.com>
2025-10-27 15:29:53 +0900

    플러터 다시요..

--- maldong_i_flutter/android/app/src/main/kotlin/com/example/maldong_i_flutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.maldong_i_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- maldong_i_flutter/lib/api/config.dart
+++
@@ -1,9 +0,0 @@
-// lib/api/config.dart
-class ApiConfig {
-  static const String baseUrl = 'http://localhost:5000';
-  static const String apiBase = 'http://localhost:5000';
-  
-  static const Map<String, String> headers = {
-    'Content-Type': 'application/json',
-  };
-}
