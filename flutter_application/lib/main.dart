4a02e2eb1a1be2656fa69eb974fdbeb12000bcc2
seoyun Jang <seoyun_j211@naver.com>
2025-11-27 18:03:29 +0900

    말동이

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -17,7 +17,7 @@ import 'font_size_provider.dart';
 import 'maldong_avatar.dart';
 
 // GLB 파일 경로
-const customAvatarUrl = 'assets/model.glb';
+const customAvatarUrl = 'assets/maldongs.glb';
 
 // 앱 전체에서 쓸 카메라 리스트
 late List<CameraDescription> cameras;
@@ -119,7 +119,7 @@ class _AppRootState extends State<AppRoot> {
     setState(() {
       _isLoggedIn = true;
       _userType = role == 'ward' ? UserType.elderly : UserType.guardian;
-      
+
       if (_userType == UserType.elderly) {
         _currentScreen = AppScreen.elderlyHome;
       } else {
